7e4b134184803a4d931b98f2dce5b01f804d95e2
Lucas Santos <lucasnascimentoss@gmail.com>
2016-06-24 18:49:28 -0300

    Arrumando a master

---
+++ MiniHaskell/src-testes/br/unb/cic/poo/testes/AllTests.java
@@ -0,0 +1,14 @@
+package br.unb.cic.poo.testes;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TesteExpressaoAND.class, TesteExpressaoAplicacao.class, TesteExpressaoDiferente.class,
+		TesteExpressaoDivisao.class, TesteExpressaoIgual.class, TesteExpressaoLet.class, TesteExpressaoMaior.class,
+		TesteExpressaoMenor.class, TesteExpressaoMultiplicacao.class, TesteExpressaoNOT.class, TesteExpressaoOR.class,
+		TesteExpressaoSoma.class, TesteExpressaoSubtracao.class })
+public class AllTests {
+
+}
--- MiniHaskell/src/br/unb/cic/poo/Teste/Parser/AllVisitor.java
+++
@@ -1,8 +0,0 @@
-package Parser;
-
-import Parser.Absyn.*;
-
-/** BNFC-Generated All Visitor */
-public interface AllVisitor<R,A> extends
-  Parser.Absyn.Exp.Visitor<R,A>
-{}
