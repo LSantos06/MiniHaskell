aabf068db69d9e02aaf0e0e4a9f81c3c11be5580
Filipe Teixeira Cardoso <filipetc.bsb@gmail.com>
2016-06-30 09:21:59 -0300

    Testes das expressoes <= e >=

--- MiniHaskell/src-testes/br/unb/cic/poo/testes/TesteExpressaoMaiorIgual.java
+++ MiniHaskell/src-testes/br/unb/cic/poo/testes/TesteExpressaoMaiorIgual.java
@@ -1,5 +1,27 @@
 package br.unb.cic.poo.testes;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.unb.cic.poo.expressoes.relacionais.ExpressaoMaiorIgual;
+import br.unb.cic.poo.valores.ValorBooleano;
+import br.unb.cic.poo.valores.ValorInteiro;
+
 public class TesteExpressaoMaiorIgual {
 
+	@Test
+	public void testMaiorIgualSimples() {
+		ValorInteiro v20  = new ValorInteiro(20);
+		ValorInteiro v10 = new ValorInteiro(10);
+		ValorInteiro v20_2 = new ValorInteiro(20);
+		
+		ExpressaoMaiorIgual maior = new ExpressaoMaiorIgual(v20, v10);
+		ExpressaoMaiorIgual igual = new ExpressaoMaiorIgual(v20,v20_2);
+		
+		ValorBooleano res1 = (ValorBooleano)maior.avaliar();
+		ValorBooleano res2 = (ValorBooleano)igual.avaliar();
+		
+		Assert.assertEquals(true, res1.getValor());
+		Assert.assertEquals(true, res2.getValor());
+	}
 }
--- MiniHaskell/src-testes/br/unb/cic/poo/testes/TesteExpressaoMenorIgual.java
+++ MiniHaskell/src-testes/br/unb/cic/poo/testes/TesteExpressaoMenorIgual.java
@@ -1,5 +1,27 @@
 package br.unb.cic.poo.testes;
 
-public class TesteExpressaoMenorIgual {
+import org.junit.Assert;
+import org.junit.Test;
 
+import br.unb.cic.poo.expressoes.relacionais.ExpressaoMenorIgual;
+import br.unb.cic.poo.valores.ValorBooleano;
+import br.unb.cic.poo.valores.ValorInteiro;
+
+public class TesteExpressaoMenorIgual {
+	
+	@Test
+	public void testMenorIgualSimples() {
+		ValorInteiro v40  = new ValorInteiro(40);
+		ValorInteiro v20 = new ValorInteiro(20);
+		ValorInteiro v20_2 = new ValorInteiro(20);
+		
+		ExpressaoMenorIgual menor = new ExpressaoMenorIgual(v40, v20);
+		ExpressaoMenorIgual igual = new ExpressaoMenorIgual(v20, v20_2);
+		
+		ValorBooleano res1 = (ValorBooleano)menor.avaliar();
+		ValorBooleano res2 = (ValorBooleano)igual.avaliar();
+		
+		Assert.assertEquals(false, res1.getValor());
+		Assert.assertEquals(true, res2.getValor());
+	}
 }
